41bb96a3d2b1e338c5c439ea6fb9c19db330d97f
Yefry Moraga <jeffamed@gmail.com>
2026-03-10 08:45:37 -0600

    add lockForUpdate to prevent product stock from going negative

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Sale;
 use App\Dtos\SalesFilter;
 use Illuminate\Container\Attributes\CurrentUser;
@@ -9,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Services\SaleService;
 use App\Http\Requests\SaleRequest;
 use App\Http\Resources\SaleResource;
+use function PHPUnit\Framework\throwException;
 
 class SaleController extends Controller
 {
@@ -28,10 +30,28 @@ class SaleController extends Controller
     {
         $data = $request->validated();
         $data['user_id'] = \Auth::id();
-        $sale = Sale::create($data);
-        if ($request->filled('details')){
-            $this->saleService->registerDetail($sale, $request->array('details'));
-        }
+        $sale = \DB::transaction(function () use ($data, $request){
+            $details = collect($request->array('details'));
+            $productIds = $details->pluck('product_id');
+            $products = Product::whereIn('id', $productIds)
+                ->lockForUpdate()
+                ->get();
+            $products->each(function ($product) use ($details) {
+                $qtyByProd = collect($details)->where('product_id', $product->id)->sum('quantity');
+                if ($product->stock < $qtyByProd){
+                    throw new \Exception("No hay suficiente stock para el producto {$product->name}");
+                }
+                $product->decrement('stock', $qtyByProd);
+                $product->box_stock = $product->stock / $product->unit_box;
+                $product->save();
+            });
+
+            $sale = Sale::create($data);
+            if ($request->filled('details')){
+                $this->saleService->registerDetail($sale, $request->array('details'));
+            }
+        });
+
         return new SaleResource($sale);
     }
 
