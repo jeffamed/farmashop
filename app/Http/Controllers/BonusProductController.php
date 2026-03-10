19f8cc97f0360032144b4a9f673e72970528ac3a
Yefry Moraga <jeffamed@gmail.com>
2026-03-10 08:12:39 -0600

    integracion de redis para cache::lock en bonus

--- app/Http/Controllers/BonusProductController.php
+++ app/Http/Controllers/BonusProductController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\BonusRequest;
 use App\Models\Product;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use App\Http\Requests\BonusRequest;
+use Illuminate\Support\Facades\Cache;
 
 class BonusProductController extends Controller
 {
     public function __invoke(BonusRequest $request, Product $product)
     {
-        $product->stock += $request->integer('quantity');
-        $product->save();
+        $lock = Cache::lock('bonusProcessing', 10);
+        try {
+            if ($lock->get()) {
+                $product->stock += $request->integer('quantity');
+                $product->save();
+            } else {
+                return response()->json(['message' => 'El recurso está siendo usado por otro proceso'], Response::HTTP_LOCKED);
+            }
 
-        return response()->json('Stock actualizado correctamente');
+        } catch (\Throwable $e) {
+            return response()->json(['message' => 'Error al actualizar el stock'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        } finally {
+            optional($lock)->release();
+        }
+        return response()->json('Stock actualizado correctamente', Response::HTTP_OK);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\LaboratoryController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\PresentationController;
+use App\Http\Controllers\ReimbursementController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\BonusProductController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReportController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function (){
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('laboratories', LaboratoryController::class);
+    Route::apiResource('locations', LocationController::class);
+    Route::apiResource('presentations', PresentationController::class);
+    Route::apiResource('reimbursements', ReimbursementController::class);
+    Route::apiResource('orders', OrderController::class);
+    Route::apiResource('bonus-products', BonusProductController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::get('reports', ReportController::class)->name('reports');
+    Route::apiResource('suppliers', \App\Http\Controllers\SupplierController::class);
+    Route::apiResource('sales', \App\Http\Controllers\SaleController::class);
+    Route::apiResource('types', \App\Http\Controllers\TypeController::class);
+    Route::apiResource('usages', \App\Http\Controllers\UsageController::class);
+    Route::apiResource('users', \App\Http\Controllers\UserController::class);
+    Route::post('close-cash', \App\Http\Controllers\CloseCashController::class)->name('close-cash');
 });
